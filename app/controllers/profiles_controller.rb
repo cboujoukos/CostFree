6e2f60deef85bd60c32cc475dfadc026e62be810
Chris Boujoukos <github email address>
2018-03-08 13:50:52 -0500

    finished profile_categories and profile forms

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,8 @@ class ProfilesController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:user_id])
+    @profile = @user.try(:profile)
+    @interests = @user.try(:profile).try(:categories)
   end
 
   def new
@@ -16,6 +18,23 @@ class ProfilesController < ApplicationController
       redirect_to user_profile_path
     else
       render "new"
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: params[:user_id])
+    @profile = @user.profile
+  end
+
+  def update
+    binding.pry
+    @user = User.find_by(id: params[:user_id])
+    @profile = @user.profile
+    @profile.update(profile_params)
+    if @profile.save
+      redirect_to user_profile_path
+    else
+      render "edit"
     end
   end
 
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,12 +3,13 @@ class Profile < ApplicationRecord
   has_many :profile_categories
   has_many :categories, through: :profile_categories
 
-  has_attached_file :icon, styles: { medium: "300x300>", thumb: "100x100>" }
-  validates_attachment_content_type :icon, content_type: /\Aimage\/.*\z/
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   def categories_attributes=(categories_attributes)
-    categories_attributes.each do |i, category_attributes|
-      if category_attributes.id != ""
+    binding.pry
+    categories_attributes.values.each do |category_attributes|
+      if category_attributes[:id] != ""
         category = Category.find_by(id: category_attributes[:id])
         if !self.categories.include?(category)
           self.profile_categories.build(
