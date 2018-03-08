98b8a9fac26ea42118e62bdc37c06ccd45daf617
Chris Boujoukos <github email address>
2018-03-08 11:43:19 -0500

    built profile form and strong params

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,6 +5,24 @@ class ProfilesController < ApplicationController
   end
 
   def new
+    @user = User.find_by(id: params[:user_id])
     @profile = Profile.new
+  end
+
+  def create
+    raise params.inspect
+    @user = User.find_by(id: params[:user_id])
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      redirect_to user_profile_path
+    else
+      render "new"
+    end
+  end
+
+
+  private
+  def profile_params
+    params.require(:profile).permit(:user_id, :username, :avatar)
   end
 end
