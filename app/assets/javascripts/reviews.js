dd42a3e48e4b49473872915c50af7ad923a97f5e
Chris Boujoukos <github email address>
2018-05-22 12:44:51 -0400

    fixed turbolinks load bug. jquery now loads on refresh and on redirect

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -10,17 +10,6 @@ class Review {
     this.user_id = attributes['user']['id']
   }
 }
-// function Review(attributes){
-//   this.id = attributes.id;
-//   this.title = attributes.title;
-//   this.rating = attributes.rating;
-//   this.comment = attributes.comment;
-//   this.updated_at = attributes.updated_at
-//   this.activity_id = attributes['activity']['id'];
-//   this.user_email = attributes['user']['email']
-//   this.user_id = attributes['user']['id']
-// }
-
 
 Review.prototype.renderReview = function(){
   return Review.template(this)
@@ -30,7 +19,7 @@ Review.prototype.renderReview = function(){
    return this.updated_at.slice(5,10).replace('-','/') + "/" + this.updated_at.slice(0,4)
  }
 
-$(function(){
+$(document).on("turbolinks:load", function(){
   Handlebars.registerPartial('reviewPartial', document.getElementById('review-partial').innerHTML);
   Handlebars.registerHelper('formatDate', function(timestamp){
      return timestamp.slice(5,10).replace('-','/') + "/" + timestamp.slice(0,4);
@@ -38,9 +27,8 @@ $(function(){
 
   Review.templateSource = $("#review-template").html();
   Review.template = Handlebars.compile(Review.templateSource);
-
-  // attachReviewListeners();
 })
+
 
 document.addEventListener("turbolinks:load", function() {
   $("form#new_review").submit(function(e){
@@ -50,32 +38,15 @@ document.addEventListener("turbolinks:load", function() {
   })
 })
 
-// function attachReviewListeners(){
-//   $("form#new_review").submit(function(e){
-//     e.preventDefault();
-//     var $form = $(this)
-//     submitNewReview($form);
-//   })
-// }
-
-
 function submitNewReview($form){
   let action = $form.attr("action");
   let params = $form.serialize()
   let posting = $.post(action, params)
   posting.done(function(data){
      console.log(data)
-    if (!data.id) {
-      console.log(typeof data)
-      let errorMessage = []
-      for (var key in data) {
-        errorMessage.push(`${key} ${data[key]}. `)
-      };
-      console.log(errorMessage.join("\n"))
-      $("#error_msg").html(errorMessage.join("\n"))
-      $("form#new_review [name=commit]").prop("disabled", false)
-    } else {
+    if (!!data.id) {
       let review = new Review(data);
+      debugger;
       let reviewCard = review.renderReview();
       $("#reviewBox").prepend(reviewCard);
       $("#timestampHolder").html(review.reformatDate());
@@ -94,26 +65,42 @@ function submitNewReview($form){
       }
       // $("#newReview").show();
       $("#review_form_placeholder").html("");
+
+    } else {
+      console.log(typeof data)
+      let errorMessage = []
+      for (var key in data) {
+        errorMessage.push(`${key} ${data[key]}. `)
+      };
+      console.log(errorMessage.join("\n"))
+      $("#error_msg").html(errorMessage.join("\n"))
+      $("form#new_review [name=commit]").prop("disabled", false)
     }
-
-    // // $("#review_form_btn").toggle();
-    // $("#reviewTitle").text(data["title"]);
-    // $("#reviewId").text(data["id"]);
-    // $(".edit_btn_placeholder").html("<a href='/activities/" + data['activity']['id'] + "/reviews/" + data['id'] + "/edit' id='edit_review_btn' role='button' class='ml-3 btn btn-outline-dark'>Edit your review</a>")
-    // $("#reviewComment").text(data["comment"]);
-    // $("#reviewUserEmail").text(data["user"]["email"]);
-    // $("#reviewUpdatedAt").text(data["updated_at"].slice(5,10).replace('-','/') + "/" + data["updated_at"].slice(0,4));
-    // let checkedStars = $("[data-value]").filter(function() {
-    //   return  $(this).attr("data-value") <= parseInt(data["rating"]);
-    // });
-    // checkedStars.addClass("checked")
-
   })
   .fail(function(e){
     alert("Sorry, something went wrong")
   })
 
 }
-// document.addEventListener("DOMContentLoaded", function(e){
-//   init()
-// })
+
+///////////////// Traditional Ready function does not work with turbolinks //////////
+$(function(){
+  Handlebars.registerPartial('reviewPartial', document.getElementById('review-partial').innerHTML);
+  Handlebars.registerHelper('formatDate', function(timestamp){
+     return timestamp.slice(5,10).replace('-','/') + "/" + timestamp.slice(0,4);
+  });
+
+  Review.templateSource = $("#review-template").html();
+  Review.template = Handlebars.compile(Review.templateSource);
+
+
+  // attachReviewListeners();
+})
+
+// function attachReviewListeners(){
+//   $("form#new_review").submit(function(e){
+//     e.preventDefault();
+//     var $form = $(this)
+//     submitNewReview($form);
+//   })
+// }
